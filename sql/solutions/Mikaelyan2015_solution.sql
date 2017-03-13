4dbe8f8517898fa7503331beb40c21aa351228f2
Stefano Allesina <sallesina@uchicago.edu>
2017-03-13 15:12:44 -0500

    Solutions SQL

---
+++ sql/solutions/Mikaelyan2015_solution.sql
@@ -0,0 +1,56 @@
+/* Gut microbiome of termites --- Mikaelyan et al. 2015 */
+/* You can copy and paste these commands into the SQLite shell */
+
+/*
+1) By joining the three tables, create a view displaying
+   Spp (species name), OTU (code for the OTU), and Num, 
+   the number of occurrences of the OTU in the gut of the
+   species. Notice that Num can be (and often is) $0$.
+*/
+
+-- First, we write a simple query to join the three tables:
+SELECT Spp, OTU, Num
+FROM tSpp INNER JOIN tNumber ON tSpp.IDSpp = tNumber.IDSpp
+INNER JOIN tOTU on tNumber.IDOTU = tOTU.IDOTU
+LIMIT 50;
+
+-- If we're satisfied with the result, we can create the view
+CREATE VIEW combined AS
+SELECT Spp, OTU, Num
+FROM tSpp INNER JOIN tNumber ON tSpp.IDSpp = tNumber.IDSpp
+INNER JOIN tOTU on tNumber.IDOTU = tOTU.IDOTU;
+
+/*
+2) Count the number of OTUs found in each species (remember to
+   filter using Num > 0).
+*/
+
+SELECT Spp, count(OTU) AS NumOTU FROM 
+combined WHERE Num >0
+GROUP BY Spp ORDER BY NumOTU DESC;
+
+/* 
+3) List the OTUs that are found in four or more species
+*/
+
+SELECT OTU, count(Spp) AS NumSpp FROM 
+combined WHERE Num >0
+GROUP BY OTU 
+HAVING NumSpp > 3
+ORDER BY NumSpp DESC;
+
+/* 
+4) Delete all the records in tNumber where Num is zero.
+*/
+
+DELETE FROM mytable
+WHERE Num = 0;
+
+/*
+5) [Advanced] Using the package vegan in R, compute the 
+   Shannon's index of diversity for the gut microbial
+   community of each species.
+*/
+
+-- The solution is contained in the file calculate_Shannon.Rmd
+
---
+++ sql/solutions/Zmihorski2015_solution.sql
@@ -0,0 +1,61 @@
+/* Species richness of birds in wetlands --- Zmihorski et al. 2015 */
+/* You can copy and paste these commands into the SQLite shell */
+
+/* 
+1) Each Area contains several sites (PointID). Calculate the average
+   Species.richness for each site.
+*/
+
+-- We need to average Species.richness while grouping by Area:
+SELECT Area, avg(Species.richness) AS avg_richness
+FROM tbirds
+GROUP BY AREA 
+ORDER BY AREA;
+-- Note that we need to put Species.richness between quotes
+-- as SQLite does not like punctuation within field names.
+
+
+/*
+2) Calculate the average Species.richness for each
+   Area/Year combination.
+*/
+
+-- As the query above, but group also by Year:
+SELECT Area, Year, avg(Species.richness) AS avg_richness
+FROM tbirds
+GROUP BY AREA, Year
+ORDER BY AREA, Year;
+
+/*
+3) Calculate whether more species are found (on average) when the
+   area is flooded (field Flooding.binary), or not.
+*/
+
+SELECT Flooding.binary, avg(Species.richness) AS avg_richness
+FROM tbirds
+GROUP BY Flooding.binary;
+
+/* 
+4) Produce a count of the co-occurrence of three bird species (Yellowwagtail, YW; 
+   Lapwing, LW; Skylark, SK), producing a table like:
+ 
+    YW  LW  SK  Occurrences
+    0	0	0	58
+    0	0	1	18
+    0	1	0	24
+    0	1	1	15
+    1	0	0	80
+    1	0	1	59
+    1	1	0	75
+    1	1	1	204    
+
+*/    
+
+SELECT (Yellowwagtail > 0) AS YW,
+(Lapwing > 0) AS LW,
+(Skylark > 0) AS SK,
+count(*) AS Occurrences
+FROM tbirds
+GROUP BY YW, LW, SK;
+
+
